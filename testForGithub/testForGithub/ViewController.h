eff7a6a893a20d9236ce18a7255149709cd242f8
XMG <XMG@163.com>
2016-04-25 16:57:27 +0800

    init
    init practise

---
+++ testForGithub/testForGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testForGithub
+//
+//  Created by Hahn on 16/4/25.
+//  Copyright © 2016年 Hahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testForGithub/testForGithub/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  testForGithub
+//
+//  Created by Hahn on 16/4/25.
+//  Copyright © 2016年 Hahn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    // this is a practise for using github
+    UIView *view = [[UIView alloc] init];
+    view.frame = CGRectMake(0, 0, 100, 100);
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
